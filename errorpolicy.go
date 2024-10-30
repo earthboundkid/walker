ce31a1ff671ebdab605c7bc92ecb97bbb51c13d3
Carlana Johnson <mail@carlana.net>
2024-10-30 17:48:00 -0400

    ErrorPolicy: Doc indirection; add OnErrorPanic

--- errorpolicy.go
+++ errorpolicy.go
@@ -11,10 +11,20 @@ import (
 type ErrorPolicy func(err error, e Entry) bool
 
 // OnErrorIgnore is an ErrorPolicy that always continues regardless of errors.
-var OnErrorIgnore ErrorPolicy = func(error, Entry) bool { return true }
+var OnErrorIgnore ErrorPolicy = onErrorIgnore
+
+func onErrorIgnore(error, Entry) bool { return true }
 
 // OnErrorHalt is an ErrorPolicy that halts on any error.
-var OnErrorHalt ErrorPolicy = func(error, Entry) bool { return false }
+var OnErrorHalt ErrorPolicy = onErrorHalt
+
+func onErrorHalt(error, Entry) bool { return false }
+
+// OnErrorPanic is an ErrorPolicy that panics on any error.
+// Not recommended for production.
+var OnErrorPanic ErrorPolicy = onErrorPanic
+
+func onErrorPanic(err error, _ Entry) bool { panic(err) }
 
 // OnErrorCollect returns an ErrorPolicy that
 // collects errors into the provided slice
